2f6d0fde1c438f705230bcfd5fd2059eeb41101c
Alireza Mahdavi <AlirezaMahDev@outlook.com>
2025-03-11 13:36:06 +0330

    fix program cs

--- src/Parstech.Shop.ApiService/Program.cs
+++ src/Parstech.Shop.ApiService/Program.cs
@@ -1,38 +1,47 @@
-using Parstech.Shop.ApiService.Data;
+using Shop.Persistence.Context;
+using Shop.Application;
+using Shop.Infrastructure;
+using Shop.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigurePersistenceService(builder.Configuration);
+builder.Services.ConfigureInfrustructureService();
+builder.Services.ConfigureApplicationService(builder.Configuration);
+
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyMethod()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:7040");
+    });
+});
 
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
-builder.Services.AddSqlServer<DataContext>("database");
+builder.Services.AddSqlServer<DatabaseContext>("database");
 
 var app = builder.Build();
 
 app.UseExceptionHandler();
+app.UseHttpsRedirection();
+app.UseMigrationsEndPoint();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseCors();
+app.UseRouting();
+app.UseAuthorization();
 
 app.MapDefaultEndpoints();
+app.MapStaticAssets();
+app.MapControllers();
 
 app.Run();
 
--- src/Parstech.Shop.Web/Program.cs
+++ src/Parstech.Shop.Web/Program.cs
@@ -3,18 +3,25 @@ using Parstech.Shop.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 builder.AddRedisOutputCache("cache");
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyMethod().WithOrigins("https://localhost:7040");
+    });
+});
+
+builder.AddServiceDefaults();
+builder.Services.AddProblemDetails();
+
 builder.Services.AddHttpClient<WeatherApiClient>(client =>
     {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
         client.BaseAddress = new("https+http://apiservice");
     });
 
@@ -23,29 +30,20 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.UseOutputCache();
 
 app.UseRouting();
 app.UseAuthorization();
 
-
-app.MapStaticAssets();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
 app.MapDefaultEndpoints();
 app.MapRazorPages();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
