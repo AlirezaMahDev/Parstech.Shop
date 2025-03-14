15a1e8047effebd063212f249f766a9463459e2e
Alireza Mahdavi <AlirezaMahDev@outlook.com>
2025-03-14 09:48:08 +0330

    c3

--- Shop.Application/Features/Product/Requests/Queries/IntegratedProductsPagingQueryReq.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using Shop.Application.DTOs.Product;
-
-
-namespace Shop.Application.Features.Product.Requests.Queries
-{
-    public record IntegratedProductsPagingQueryReq(ProductSearchParameterDto parameters,string userName) :IRequest<ProductPageingDto>;
-    
-}
